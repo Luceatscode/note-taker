620c6e8ec381fefd016fe1d9c3db66c2975c4a6c
Luceatscode <lucsw92@hotmail.com>
2023-06-24 10:33:18 -0600

    served notes.html

--- Develop/server/index.js
+++ Develop/server/index.js
@@ -2,14 +2,19 @@ const express = require('express')
 const app = express()
 const port = 3000
 const path = require('path');
-const staticPath = path.join(__dirname,'..','public');
+const staticPath = path.join(__dirname, '..', 'public');
 
 app.use(express.static(staticPath));
 app.get('/', (req, res) => {
     const filePath = path.join(staticPath, 'index.html');
     res.sendFile(filePath);
-  });
+});
+
+app.get('/notes', (req, res) => {
+    const filePath = path.join(staticPath, 'notes.html');
+    res.sendFile(filePath);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
