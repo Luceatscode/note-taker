ddd752ddc93edef2e7fdfcce2e1351f4797b4788
Luceatscode <lucsw92@hotmail.com>
2023-06-24 11:07:06 -0600

    get api reading from db json

--- Develop/server/index.js
+++ Develop/server/index.js
@@ -3,6 +3,10 @@ const app = express()
 const port = 3000
 const path = require('path');
 const staticPath = path.join(__dirname, '..', 'public');
+const fs = require('fs');
+const filePath = path.join(__dirname, '..', 'db', 'db.json');
+console.log('cheetoh',filePath);
+
 
 app.use(express.static(staticPath));
 app.get('/', (req, res) => {
@@ -13,6 +17,22 @@ app.get('/', (req, res) => {
 app.get('/notes', (req, res) => {
     const filePath = path.join(staticPath, 'notes.html');
     res.sendFile(filePath);
+});
+
+app.get('/api/notes', (req, res) => {
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        const jsonData = JSON.parse(data);
+        res.json(jsonData);
+      });
+      
+});
+
+app.post('/api/notes', (req, res) => {
+
 });
 
 app.listen(port, () => {
