da1205859e486e1b3745f003f9188533a477b6ac
Luceatscode <lucsw92@hotmail.com>
2023-06-24 10:29:25 -0600

    serving html file

--- Develop/server/index.js
+++ Develop/server/index.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const path = require('path');
+const staticPath = path.join(__dirname,'..','public');
 
+app.use(express.static(staticPath));
 app.get('/', (req, res) => {
-  res.send('Hello Joey!')
-})
+    const filePath = path.join(staticPath, 'index.html');
+    res.sendFile(filePath);
+  });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
