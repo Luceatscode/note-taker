c6238ba3358ec7e83d1d472f693bedd773c57310
Luceatscode <lucsw92@hotmail.com>
2023-06-24 11:40:57 -0600

    add post route to save notes

--- Develop/server/index.js
+++ Develop/server/index.js
@@ -5,10 +5,11 @@ const path = require('path');
 const staticPath = path.join(__dirname, '..', 'public');
 const fs = require('fs');
 const filePath = path.join(__dirname, '..', 'db', 'db.json');
-console.log('cheetoh',filePath);
+console.log('cheetoh', filePath);
 
 
 app.use(express.static(staticPath));
+app.use(express.json());
 app.get('/', (req, res) => {
     const filePath = path.join(staticPath, 'index.html');
     res.sendFile(filePath);
@@ -22,19 +23,38 @@ app.get('/notes', (req, res) => {
 app.get('/api/notes', (req, res) => {
     fs.readFile(filePath, 'utf8', (err, data) => {
         if (err) {
-          console.error(err);
-          return;
+            console.error(err);
+            return;
         }
         const jsonData = JSON.parse(data);
         res.json(jsonData);
-      });
-      
+    });
+
 });
 
 app.post('/api/notes', (req, res) => {
+    const body = req.body;
+    const jsonBody = JSON.stringify(body, null, 2);
 
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        const jsonData = JSON.parse(data);
+        const responseData = JSON.stringify([...jsonData, req.body]);
+        // res.json(responseData);
+
+        fs.writeFile(filePath, responseData, 'utf8', (err) => {
+            if (err) {
+                console.error(err);
+                res.status(500).send('Error writing to JSON file');
+                return;
+            }
+        });
+    });
 });
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+    app.listen(port, () => {
+        console.log(`Example app listening on port ${port}`)
+    });
